39ed353672c8e217862ff169d0f0f4754efc13d6
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-03 21:40:53 +0530

    [chiru] EmployeeWages Check Using Switch Statement

--- EmployeeWageProblems/EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/EmployeeWageProblems/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWageProblems;
 
-Console.WriteLine("Welcome To EmployeeWage Computation Problems");
+//Console.WriteLine("Welcome To EmployeeWage Computation Problems");
 
 //UC1_EmployeePresentOrAbsent.CheckEmployeeStatus();
 //UC2_DailyEmployeeWage.DailyWage();
-UC3_PartTimeWage.PartTimeWages();
+//UC3_PartTimeWage.PartTimeWages();
+UC4_SwitchCase_Statement.EmployeeWagesUsingSwitch();
